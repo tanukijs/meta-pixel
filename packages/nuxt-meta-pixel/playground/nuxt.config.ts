e15f9852444ba36973f8fbbfb59d018043beb06e
Igor Halfeld <hello@igorluiz.me>
2024-05-06 01:13:22 -0300

    feat: update examples with new approach

--- packages/nuxt-meta-pixel/playground/nuxt.config.ts
+++ packages/nuxt-meta-pixel/playground/nuxt.config.ts
@@ -1,5 +1,10 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  nuxtMetaPixel: {},
+  nuxtMetaPixel: {
+    pixelId: '415215247513663',
+    pixels: [
+      { pixelId: '415215247513664', routes: ['/about/**'] }
+    ]
+  },
   devtools: { enabled: true }
 })
