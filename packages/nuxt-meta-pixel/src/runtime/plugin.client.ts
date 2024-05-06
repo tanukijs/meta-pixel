c4e4f8ea6d0c5fa2a181f897bcd52d8f005eaf7d
Igor Halfeld <hello@igorluiz.me>
2024-05-06 01:16:13 -0300

    feat: support to multiple pixels

--- packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
+++ packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
@@ -1,20 +1,49 @@
+import { Minimatch } from 'minimatch'
 import { defineNuxtPlugin, useRuntimeConfig, useRouter } from '#app'
 import { addScript } from 'meta-pixel'
-import type { ModuleOptions } from '../module'
+import type { Pixel } from '../types'
 
-export default defineNuxtPlugin((nuxtApp) => {
+
+function getMatchingPixel (pixels: Pixel[], path: string) {
+  if (!pixels) {
+    return
+  }
+
+  const matched = pixels.find(pixel => {
+    const routes = pixel.routes ?? []
+
+    const routeIndex = routes.findIndex(route => {
+      const minimatch = new Minimatch(route)
+      return minimatch.match(path)
+    })
+
+    return routeIndex !== -1
+  })
+
+  return matched?.pixelId
+}
+
+export default defineNuxtPlugin(async (nuxtApp) => {
   const runtimeConfig = useRuntimeConfig()
   const router = useRouter()
   const fbq = addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
   const opts = runtimeConfig.public.nuxtMetaPixel as ModuleOptions
 
+  router.afterEach(({ path }) => {
+    const matchingPixelId = getMatchingPixel(opts.pixels, path)
+    if (!matchingPixelId) {
+      return
+    }
+
+    fbq('set', 'autoConfig', opts.autoConfig, matchingPixelId)
+    fbq('init', matchingPixelId)
+    fbq('track', 'PageView')
+  })
+
   fbq('set', 'autoConfig', opts.autoConfig, opts.pixelId)
   fbq('init', opts.pixelId)
   fbq('track', 'PageView')
 
-  router.afterEach(() => {
-    fbq('track', 'PageView')
-  })
 
   nuxtApp.provide('fbq', fbq)
 })
