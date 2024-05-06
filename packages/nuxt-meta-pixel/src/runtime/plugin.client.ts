dec57e3026c3b74fbb27ec3f1ef01fbbb1102332
Igor Halfeld <hello@igorluiz.me>
2024-05-06 19:12:05 -0300

    chore: use trackSingle to handle track to multiple loaded pixels

--- packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
+++ packages/nuxt-meta-pixel/src/runtime/plugin.client.ts
@@ -1,23 +1,31 @@
 import { Minimatch } from 'minimatch'
 import { defineNuxtPlugin, useRuntimeConfig, useRouter } from '#app'
 import { addScript } from 'meta-pixel'
-import type { Pixel } from '../types'
+import type { ModuleOptions, Pixel } from '../types'
 
 
-function getMatchingPixel (pixels: Pixel[], path: string) {
-  if (!pixels) {
+interface MatchCurrentPixelOptions {
+  pixels: Pixel[]
+  path: string
+}
+
+// @TODO: maybe this helper function should be
+// moved to a helper package to share across
+// framework implementations
+function matchCurrentPixel({ pixels, path }: MatchCurrentPixelOptions): string | undefined {
+  if (!pixels.length) {
     return
   }
 
   const matched = pixels.find(pixel => {
     const routes = pixel.routes ?? []
 
-    const routeIndex = routes.findIndex(route => {
+    const route = routes.find(route => {
       const minimatch = new Minimatch(route)
       return minimatch.match(path)
     })
 
-    return routeIndex !== -1
+    return Boolean(route)
   })
 
   return matched?.pixelId
@@ -26,11 +34,15 @@ function getMatchingPixel (pixels: Pixel[], path: string) {
 export default defineNuxtPlugin(async (nuxtApp) => {
   const runtimeConfig = useRuntimeConfig()
   const router = useRouter()
-  const fbq = addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+  const fbq = await addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
   const opts = runtimeConfig.public.nuxtMetaPixel as ModuleOptions
 
   router.afterEach(({ path }) => {
-    const matchingPixelId = getMatchingPixel(opts.pixels, path)
+    const matchingPixelId = matchCurrentPixel({
+      path,
+      pixels: opts.pixels ?? [],
+    })
+
     if (!matchingPixelId) {
       fbq('track', 'PageView')
       return
@@ -38,13 +50,12 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 
     fbq('set', 'autoConfig', opts.autoConfig, matchingPixelId)
     fbq('init', matchingPixelId)
-    fbq('track', 'PageView')
+    fbq('trackSingle', matchingPixelId, 'PageView')
   })
 
   fbq('set', 'autoConfig', opts.autoConfig, opts.pixelId)
   fbq('init', opts.pixelId)
-  fbq('track', 'PageView')
-
+  fbq('trackSingle', opts.pixelId, 'PageView')
 
   nuxtApp.provide('fbq', fbq)
 })
