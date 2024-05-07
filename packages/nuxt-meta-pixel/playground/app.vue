328fb8c8dd2492b4032a7d099994b4e04e5e3c19
Tanuki <tanuki.contact@gmail.com>
2024-05-08 00:09:53 +0200

    meta-pixel 0.0.2 & nuxt-meta-pixel 1.0.1 (#5)
    Fixes

--- packages/nuxt-meta-pixel/playground/app.vue
+++ packages/nuxt-meta-pixel/playground/app.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 const { $fbq } = useNuxtApp()
+const YOUR_PIXEL_ID = ''
+
 $fbq('track', 'CompleteRegistration')
 $fbq('trackSingle', YOUR_PIXEL_ID, 'CompleteRegistration')
+$fbq('track', 'AddToCart')
 </script>
 
 <template>
