72d6639b47caf95398aa8b1c584f081d1a49719a
Tanuki <tanuki.contact@gmail.com>
2024-05-13 20:22:51 +0200

    Setup improvements

--- packages/meta-pixel/README.md
+++ packages/meta-pixel/README.md
@@ -6,15 +6,53 @@
 
 > TypeScript implementation of the facebook's pixel script. This is a client-side only library.
 
+<img src="https://raw.githubusercontent.com/tanukijs/meta-pixel/dev/events.png" style="max-width: 400px" />
+
 ## Usage
+### Manually setup pixels
+```ts
+import { addScriptDefault } from 'meta-pixel'
 
-### Load a single pixel
-```js
-import { initSingle } from 'meta-pixel'
-
-const fbq = initSingle('my_pixel_id')
-// fbq('track', 'CompleteRegistration')
+const fbq = addScriptDefault()
+fbq('set', 'autoConfig', 'pixel_01', true)
+fbq('init', 'pixel_01')
+fbq('track', 'PageView')
 ```
+
+### Using setup & multi pixels
+```ts
+import { setup } from 'meta-pixel'
+
+const { $fbq } = setup()
+  .init('pixel_01')
+  .init('pixel_02', false)
+  .pageView()
+
+$fbq('track', 'CompleteRegistration')
+```
+
+### Combining both
+```ts
+import { addScriptDefault, setup } from 'meta-pixel'
+
+const fbq = addScriptDefault()
+setup(fbq)
+  .init('pixel_01')
+  .init('pixel_02')
+  .pageView()
+
+fbq('track', 'CompleteRegistration')
+```
+
+### Manually define fbevents script
+```diff
+- const fbq = metapixel.addScriptDefault()
++ const fbq = metapixel.addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+```
+
+## Resources
+- https://developers.facebook.com/docs/meta-pixel/get-started/
+- https://developers.facebook.com/docs/meta-pixel/advanced/#automatic-configuration
 
 <!-- Badges -->
 [npm-version-src]: https://img.shields.io/npm/v/meta-pixel/latest.svg?style=flat&colorA=020420&colorB=00DC82
--- packages/meta-pixel/src/meta-pixel.ts
+++ packages/meta-pixel/src/meta-pixel.ts
@@ -1,101 +1,4 @@
-interface InitData {
-  em?: string
-  fn?: string
-  ln?: string
-  ph?: number
-  external_id?: string
-  ge?: '' | 'f' | 'm'
-  db?: number
-  ct?: string
-  st?: string
-  zp?: string
-  country?: string
-}
-
-interface FacebookQueryExtra {
-  eventID: string
-}
-
-interface EventData {
-  content_category?: string
-  content_ids?: Array<string | number>
-  content_name?: string
-  content_type?: string
-  contents?: [{ name: string, quantity: number }]
-  currency?: string
-  num_items?: number
-  predicted_ltv?: number
-  search_string?: string
-  status?: boolean
-  value?: number
-}
-
-type AddPaymentInfoData = Pick<EventData, 'content_category' | 'content_ids' | 'contents' | 'currency' | 'value'>
-type AddToCartData = Pick<EventData, 'content_ids' | 'content_name' | 'content_type' | 'contents' | 'currency' | 'value'>
-type AddToWishlist = Pick<EventData, 'content_name' | 'content_category' | 'content_ids' | 'contents' | 'currency' | 'value'>
-type CompleteRegistrationData = Pick<EventData, 'content_name' | 'currency' | 'status' | 'value'>
-type InitiateCheckoutData = Pick<EventData, 'content_category' | 'content_ids' | 'contents' | 'currency' | 'num_items' | 'value'>
-type LeadData = Pick<EventData, 'content_category' | 'content_name' | 'currency' | 'value'>
-type PurchaseData = Pick<EventData, 'content_ids' | 'content_name' | 'content_type' | 'contents' | 'num_items'> & Required<Pick<EventData, 'currency' | 'value'>>
-type ScheduleData = Pick<EventData, 'content_category' | 'content_ids' | 'content_type' | 'contents' | 'currency' | 'search_string' | 'value'>
-type StartTrialData = Pick<EventData, 'currency' | 'predicted_ltv' | 'value'>
-type SubscribeData = Pick<EventData, 'currency' | 'predicted_ltv' | 'value'>
-type ViewContentData = Pick<EventData, 'content_ids' | 'content_category' | 'content_name' | 'content_type' | 'contents' | 'currency' | 'value'>
-
-// @see https://developers.facebook.com/docs/meta-pixel/reference/
-export interface FacebookQuery {
-  (command: 'init', pixelId: string, data?: InitData): void
-  (command: 'set', key: string, value1: any, value2: any): void
-  (command: 'track', event: 'AddPaymentInfo', data?: AddPaymentInfoData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'AddToCart', data?: AddToCartData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'AddToWishlist', data?: AddToWishlist, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'CompleteRegistration', data?: CompleteRegistrationData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Contact', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'CustomizeProduct', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Donate', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'FindLocation', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'InitiateCheckout', data?: InitiateCheckoutData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Lead', data?: LeadData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'PageView'): void
-  (command: 'track', event: 'Purchase', data: PurchaseData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Schedule', data: ScheduleData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Search', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'StartTrial', data?: StartTrialData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'SubmitApplication', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'Subscribe', data?: SubscribeData, extra?: FacebookQueryExtra): void
-  (command: 'track', event: 'ViewContent', data?: ViewContentData, extra?: FacebookQueryExtra): void
-  (command: 'trackCustom', event: string, data?: any): void
-  (command: 'trackSingle', pixelId: string, event: 'AddPaymentInfo', data?: AddPaymentInfoData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'AddToCart', data?: AddToCartData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'AddToWishlist', data?: AddToWishlist, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'CompleteRegistration', data?: CompleteRegistrationData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Contact', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'CustomizeProduct', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Donate', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'FindLocation', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'InitiateCheckout', data?: InitiateCheckoutData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Lead', data?: LeadData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'PageView'): void
-  (command: 'trackSingle', pixelId: string, event: 'Purchase', data: PurchaseData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Schedule', data: ScheduleData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Search', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'StartTrial', data?: StartTrialData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'SubmitApplication', data?: Record<string, any>, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'Subscribe', data?: SubscribeData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingle', pixelId: string, event: 'ViewContent', data?: ViewContentData, extra?: FacebookQueryExtra): void
-  (command: 'trackSingleCustom', pixelId: string, event: string, data?: any): void
-  (command: 'send', ...args: any[]): void
-  (command: 'on', ...args: any[]): void
-  (command: 'loadPlugin', ...args: any[]): void
-  (command: 'dataProcessingOptions', ...args: any[]): void
-}
-
-declare global {
-  interface Window {
-    fbq?: FacebookQuery
-    _fbq?: FacebookQuery
-  }
-}
+import type { FacebookQuery, Setup } from './typings'
 
 export function addScript(f: Window, b: Document, e: string, v: string, n?: any, t?: HTMLScriptElement, s?: HTMLScriptElement): FacebookQuery {
   if (f.fbq) { return f.fbq }
@@ -127,15 +30,25 @@ export function addScript(f: Window, b: Document, e: string, v: string, n?: any,
   return n
 }
 
-/**
- * @see {@link https://developers.facebook.com/docs/meta-pixel/get-started/}
- * @see {@link https://developers.facebook.com/docs/meta-pixel/advanced/#automatic-configuration}
- */
-export function initSingle(pixelId: string, autoConfig: boolean = true) {
-  const fbq = addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
-  fbq('set', 'autoConfig', autoConfig, pixelId)
-  fbq('init', pixelId)
-  fbq('track', 'PageView')
+export function addScriptDefault() {
+  return addScript(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+}
 
-  return fbq
+export function setup($fbq: FacebookQuery = addScriptDefault()): Setup {
+  function init (pixelId: string, autoconfig: boolean = true) {
+    $fbq('set', 'autoConfig', autoconfig, pixelId)
+    $fbq('init', pixelId)
+    return setup($fbq)
+  } 
+  
+  function pageView () {
+    $fbq('track', 'PageView')
+    return { $fbq }
+  }
+
+  return {
+    $fbq,
+    init,
+    pageView
+  }
 }
